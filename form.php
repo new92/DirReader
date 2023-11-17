341d1b66d18e8b3cd2110548436b5786be888cc9
new92 <spyrospanagiotou1@gmail.com>
2023-11-17 18:52:16 +0200

    Add files via upload

---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DirReader</title>
+</head>
+<body>
+    <form method='post' action='main.php'>
+        Starting dir:
+        <input type='text' name='dir' />
+        <br /><br />
+        <input type='submit' name='submit' value='Submit' />
+    </form>
+</body>
+</html>
